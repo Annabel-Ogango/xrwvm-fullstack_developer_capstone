d71a3994c3272b6f6fa59be13191d0eca9b9202e
Annabel Ogango <85584493+Annabel-Ogango@users.noreply.github.com>
2026-03-30 11:48:22 +0300

    Comment out unused imports in views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth import logout, login, authenticate
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
+#from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
